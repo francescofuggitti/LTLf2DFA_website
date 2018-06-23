d9dfbf5906ebcdaaad5782c325aea7d6705d1cd3
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-06-23 08:00:16 +0200

    added ltlf2dfa engine and modified templates and the main file

--- ltlf2dfa.py
+++ ltlf2dfa.py
@@ -1,10 +1,79 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+#from tool.Translator import Translator
+#import subprocess
+#import random
 
 app = Flask (__name__)
 
-@app.route ('/')
+'''
+@app.route ('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        params = request.form
+        declare_assumption = False
+        if params['declare']:
+            declare_assumption = True
+        else:
+            if params['formula']:
+                try:
+                    ## TRANSLATOR ##
+                    translator = Translator(params['formula'])
+                    translator.formula_parser()
+                    translator.translate()
+                    translator.createMonafile(declare_assumption)
+
+                    ## CALL MONA TOOL ##
+                    if os.path.isfile("mona") and os.access("mona",
+                                                            os.X_OK):  # check if mona exists and if it's executable
+                        random_number = random.randrange(0,9999)
+                        automa_name = 'automa-'+str(random_number)
+                        subprocess.call('./mona -u -gw tool/automa.mona > images/'+automa_name+'.dot', shell=True)
+                    else:
+                        print('[ERROR] - MONA tool does not exist or it is not executable...')
+                        exit()
+
+                    ## POST-PROCESS MONA AUTOMATON ##
+                    dot_handler = DotHandler('images/'+automa_name+'.dot')
+                    dot_handler.modify_dot()
+                    dot_handler.output_dot()
+
+                    if os.path.isfile("mona"):  # check if automa exists
+                        subprocess.call('dot -Tgif images/'+automa_name+'.dot -o images/'+automa_name+'.gif', shell=True)
+                    else:
+                        print('[ERROR] - MONA tool does not exist or it is not executable...')
+                        exit()
+
+
+                    render_template('result.html', automa_name=automa_name)
+
+                except Exception as e:
+                    return str(e)
+            else:
+                return render_template("index.html")
+
+    else:
+        return render_template("index.html")
+'''
+
+@app.route ('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        params = request.form
+
+        if params['formula']:
+            try:
+                automa_name = "automa.jpg"
+
+                return render_template('result.html', automa_name=automa_name)
+
+            except Exception as e:
+                return str(e)
+        else:
+            return render_template("index.html")
+
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 if __name__== "__main__":
     app.run(debug=True)
