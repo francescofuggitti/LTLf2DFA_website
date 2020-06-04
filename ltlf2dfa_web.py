5d8ccd28fbe6413cfe821f00ea4b8a632b79ac72
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-06-04 22:25:07 +0200

    change layout, refactor html parts, introduce some custom css

--- ltlf2dfa_web.py
+++ ltlf2dfa_web.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-from ltlf2dfa.Translator import Translator
-from ltlf2dfa.DotHandler import DotHandler
+from flask import Flask, render_template, request, jsonify
+from ltlf2dfa.parser.ltlf import LTLfParser
+from ltlf2dfa.parser.pltlf import PLTLfParser
 import subprocess
 import os
 import datetime
@@ -19,56 +19,48 @@ def encode_svg(file):
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
 
 
-@app.route('/dfa/<string:formula>/<string:flag>', methods=['GET', 'POST'])
-def dfa(formula, flag):
-    if request.method == 'POST':
-        # formula = request.args.get('formula')
-        # flag = request.args.get('flag')
-        declare_assumption = False
-        if flag == "true":
-            declare_assumption = True
-        if formula:
-            try:
-                ## TRANSLATOR ##
-                translator = Translator(formula)
-                translator.formula_parser()
-                translator.translate()
-                translator.createMonafile(declare_assumption)
-                translator.invoke_mona()
+@app.route('/dfa', methods=['POST'])
+def dfa():
+    formula = request.form['formula']
+    if formula:
+        try:
+            ## TRANSLATOR ##
+            translator = Translator(formula)
+            translator.formula_parser()
+            translator.translate()
+            translator.createMonafile(declare_assumption)
+            translator.invoke_mona()
 
-                dot_handler = DotHandler()
-                dot_handler.modify_dot()
+            dot_handler = DotHandler()
+            dot_handler.modify_dot()
 
-                automa_name = str(datetime.datetime.now()).replace(" ", "_") + "_" + str(uuid.uuid4())
+            automa_name = str(datetime.datetime.now()).replace(" ", "_") + "_" + str(uuid.uuid4())
 
-                dot_handler.output_dot('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
-                subprocess.call('dot -Tsvg /var/www/ltlf2dfa_web/static/dot/'+ automa_name +'.dot -o /var/www/ltlf2dfa_web/static/tmp/'+automa_name+'.svg', shell=True)
+            dot_handler.output_dot('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
+            subprocess.call('dot -Tsvg /var/www/ltlf2dfa_web/static/dot/'+ automa_name +'.dot -o /var/www/ltlf2dfa_web/static/tmp/'+automa_name+'.svg', shell=True)
 
-                # automa_name = "declare-img1"
+            # automa_name = "declare-img1"
 
-                encoding = encode_svg('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name)).decode("utf-8")
-                data = {'code': "SUCCESS", 'formula': formula, 'flag': declare_assumption, 'svg': encoding}
-                json_data = json.dumps(data)
+            encoding = encode_svg('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name)).decode("utf-8")
+            data = {'code': "SUCCESS", 'formula': formula, 'flag': declare_assumption, 'svg': encoding}
+            json_data = json.dumps(data)
 
-                os.unlink('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
-                os.unlink('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name))
+            os.unlink('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
+            os.unlink('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name))
 
-                return json_data
+            return json_data
 
-                # return render_template('result.html', automa_name=automa_name, formula=params['formula'], encoding=encoding, flag=declare_assumption)
+            # return render_template('result.html', automa_name=automa_name, formula=params['formula'], encoding=encoding, flag=declare_assumption)
 
-            except Exception as e:
-                data = {'code': "FAIL", 'formula': formula, 'flag': declare_assumption, 'error': str(e)}
-                return json.dumps(data)
-                # return str(e)
-        else:
-            return render_template("index.html")
+        except Exception as e:
+            data = {'code': "FAIL", 'formula': formula, 'flag': declare_assumption, 'error': str(e)}
+            return json.dumps(data)
+            # return str(e)
     else:
-        return render_template("index.html")
-
+        return render_template("content.html")
 
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
