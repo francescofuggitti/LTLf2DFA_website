67851b800b8d8e0253b3499daaf1f37a65642319
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-06-05 11:31:29 +0200

    revise margins, introduce pages for ltlf and pltlf syntax

--- ltlf2dfa_web.py
+++ ltlf2dfa_web.py
@@ -19,48 +19,58 @@ def encode_svg(file):
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
-@app.route('/dfa', methods=['POST'])
-def dfa():
-    formula = request.form['formula']
-    if formula:
-        try:
-            ## TRANSLATOR ##
-            translator = Translator(formula)
-            translator.formula_parser()
-            translator.translate()
-            translator.createMonafile(declare_assumption)
-            translator.invoke_mona()
+@app.route('/ltlf_syntax')
+def ltlf_syntax():
+    return render_template("ltlf_syntax.html")
 
-            dot_handler = DotHandler()
-            dot_handler.modify_dot()
 
-            automa_name = str(datetime.datetime.now()).replace(" ", "_") + "_" + str(uuid.uuid4())
+@app.route('/pltlf_syntax')
+def pltlf_syntax():
+    return render_template("pltlf_syntax.html")
 
-            dot_handler.output_dot('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
-            subprocess.call('dot -Tsvg /var/www/ltlf2dfa_web/static/dot/'+ automa_name +'.dot -o /var/www/ltlf2dfa_web/static/tmp/'+automa_name+'.svg', shell=True)
 
-            # automa_name = "declare-img1"
-
-            encoding = encode_svg('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name)).decode("utf-8")
-            data = {'code': "SUCCESS", 'formula': formula, 'flag': declare_assumption, 'svg': encoding}
-            json_data = json.dumps(data)
-
-            os.unlink('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
-            os.unlink('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name))
-
-            return json_data
-
-            # return render_template('result.html', automa_name=automa_name, formula=params['formula'], encoding=encoding, flag=declare_assumption)
-
-        except Exception as e:
-            data = {'code': "FAIL", 'formula': formula, 'flag': declare_assumption, 'error': str(e)}
-            return json.dumps(data)
-            # return str(e)
-    else:
-        return render_template("content.html")
+# @app.route('/dfa', methods=['POST'])
+# def dfa():
+#     formula = request.form['formula']
+#     if formula:
+#         try:
+#             ## TRANSLATOR ##
+#             translator = Translator(formula)
+#             translator.formula_parser()
+#             translator.translate()
+#             translator.createMonafile(declare_assumption)
+#             translator.invoke_mona()
+#
+#             dot_handler = DotHandler()
+#             dot_handler.modify_dot()
+#
+#             automa_name = str(datetime.datetime.now()).replace(" ", "_") + "_" + str(uuid.uuid4())
+#
+#             dot_handler.output_dot('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
+#             subprocess.call('dot -Tsvg /var/www/ltlf2dfa_web/static/dot/'+ automa_name +'.dot -o /var/www/ltlf2dfa_web/static/tmp/'+automa_name+'.svg', shell=True)
+#
+#             # automa_name = "declare-img1"
+#
+#             encoding = encode_svg('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name)).decode("utf-8")
+#             data = {'code': "SUCCESS", 'formula': formula, 'flag': declare_assumption, 'svg': encoding}
+#             json_data = json.dumps(data)
+#
+#             os.unlink('/var/www/ltlf2dfa_web/static/dot/'+automa_name+'.dot')
+#             os.unlink('/var/www/ltlf2dfa_web/static/tmp/{}.svg'.format(automa_name))
+#
+#             return json_data
+#
+#             # return render_template('result.html', automa_name=automa_name, formula=params['formula'], encoding=encoding, flag=declare_assumption)
+#
+#         except Exception as e:
+#             data = {'code': "FAIL", 'formula': formula, 'flag': declare_assumption, 'error': str(e)}
+#             return json.dumps(data)
+#             # return str(e)
+#     else:
+#         return render_template("index.html")
 
 if __name__== "__main__":
     app.run(debug=True)
